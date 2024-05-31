084ebfceb783d9da27113fdb1f40ef44aed5a0d2
Adora Vaz <adora.vaz@gmail.com>
2024-05-31 12:01:52 -0700

    Added jpeg elevator icons

--- code/createEntrancesAndElevators.js
+++ code/createEntrancesAndElevators.js
@@ -7,20 +7,41 @@ const scale = 10000
 
 const entrancesAndElevatorsGroup = new THREE.Group();
 
+// Load textures
+const elevatorTexture = new THREE.TextureLoader().load('/elevator.jpeg');
+const entranceTexture = new THREE.TextureLoader().load('/entrance.jpeg');
+
+// function addElevator(feature) {
+//     const geometry = new THREE.CircleGeometry(0.2, 32);
+//     const material = elevatorMaterials[feature.properties.accessibility] || elevatorMaterials.no_access;
+//     const elevator = new THREE.Mesh(geometry, material);
+//     const position = new THREE.Vector2(feature.geometry.coordinates[0] - center[0], feature.geometry.coordinates[1] - center[1]).multiplyScalar(scale);
+//     elevator.position.set(position.x, 0, position.y);
+//     entrancesAndElevatorsGroup.add(elevator);
+// }
 function addElevator(feature) {
-    const geometry = new THREE.CircleGeometry(0.2, 32);
-    const material = elevatorMaterials[feature.properties.accessibility] || elevatorMaterials.no_access;
-    const elevator = new THREE.Mesh(geometry, material);
+    const material = new THREE.SpriteMaterial({ map: elevatorTexture });
+    const elevator = new THREE.Sprite(material);
+    elevator.scale.set(1, 1, 1); 
     const position = new THREE.Vector2(feature.geometry.coordinates[0] - center[0], feature.geometry.coordinates[1] - center[1]).multiplyScalar(scale);
     elevator.position.set(position.x, 0, position.y);
     entrancesAndElevatorsGroup.add(elevator);
 }
 
+// function addEntrance(feature) {
+//     const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+//     const type = feature.properties.type || 'secondary';  // Default to 'secondary' if type is not specified
+//     const material = entranceMaterials[type];
+//     const entrance = new THREE.Mesh(geometry, material);
+//     const position = new THREE.Vector2(feature.geometry.coordinates[0] - center[0], feature.geometry.coordinates[1] - center[1]).multiplyScalar(scale);
+//     entrance.position.set(position.x, 0, position.y);
+//     entrancesAndElevatorsGroup.add(entrance);
+// }
+
 function addEntrance(feature) {
-    const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
-    const type = feature.properties.type || 'secondary';  // Default to 'secondary' if type is not specified
-    const material = entranceMaterials[type];
-    const entrance = new THREE.Mesh(geometry, material);
+    const material = new THREE.SpriteMaterial({ map: entranceTexture });
+    const entrance = new THREE.Sprite(material);
+    entrance.scale.set(1.3, 1.3, 1); 
     const position = new THREE.Vector2(feature.geometry.coordinates[0] - center[0], feature.geometry.coordinates[1] - center[1]).multiplyScalar(scale);
     entrance.position.set(position.x, 0, position.y);
     entrancesAndElevatorsGroup.add(entrance);
